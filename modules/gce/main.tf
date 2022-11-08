48a6e803128dfc0defc89bcb356819556f25310c
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-11-08 18:04:07 -0300

    change module version

--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -18,6 +18,7 @@ data "google_compute_subnetwork" "vpc_subnetwork" {
 resource "google_compute_disk" "default" {
   project = var.project
   name = "compute-disk"
+  zone = self_link
 }
 
 resource "google_compute_instance" "default" {
