63cb60eb2d6fc337a3386331a6fc59545ee92fac
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-11-08 15:23:28 -0300

    new version

--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -1,51 +1,49 @@
-provider "google" {
-  project             = var.project
-  region              = var.region
-  alias               = "admin-network"
-}
-data "google_compute_network" "vpc_shared" {
-  name                    = var.vpc_network
-  provider                = google.admin-network
-  project                 = var.project
-}
+  provider "google" {
+    project             = var.project
+    region              = var.region
+    alias               = "admin-network"
+  }
+  data "google_compute_network" "vpc_shared" {
+    name                    = var.vpc_network
+    provider                = google.admin-network
+    project                 = var.project
+  }
 
-data "google_compute_subnetwork" "vpc_subnetwork" {
-    project           = var.project
-    name              = var.vpc_subnetwork
-    region            = var.region
-}
+  data "google_compute_subnetwork" "vpc_subnetwork" {
+      project           = var.project
+      name              = var.vpc_subnetwork
+      region            = var.region
+  }
 
-resource "google_compute_attached_disk" "default" {
-  disk     = google_compute_disk.default.id
-  instance = google_compute_instance.default.id
-}
+  resource "google_compute_attached_disk" "default" {
+    disk     = google_compute_disk.default.id
+    instance = google_compute_instance.default.id
+  }
 
-resource "google_compute_instance" "compute_instance" {
-  project             = var.project
-  name                = var.compute_name
-  machine_type        = var.machine_type
-  zone                = var.zone
-    boot_disk {
-      initialize_params {
-          image       = var.image
-          size        = var.disk_size
-          type        = var.disk_type
+  resource "google_compute_instance" "compute_instance" {
+    project             = var.project
+    name                = var.compute_name
+    machine_type        = var.machine_type
+    zone                = var.zone
+      boot_disk {
+        initialize_params {
+            image       = var.image
+            size        = var.disk_size
+            type        = var.disk_type
+        }
       }
-    }
-  network_interface {
-    network           = "${data.google_compute_network.vpc_shared.self_link}"
-    subnetwork        = "${data.google_compute_subnetwork.vpc_subnetwork.self_link}"
-    
-    dynamic "access_config" {
-       for_each = var.external_ip == false ? [] : [1]
-       content {
-        //
-       }
+    network_interface {
+      network           = "${data.google_compute_network.vpc_shared.self_link}"
+      subnetwork        = "${data.google_compute_subnetwork.vpc_subnetwork.self_link}"
+      
+      dynamic "access_config" {
+        for_each = var.external_ip == false ? [] : [1]
+        content {
+          //
+        }
 
-  lifecycle {
-    ignore_changes = [attached_disk]
-  }
-}   
+    lifecycle {
+      ignore_changes = [attached_disk]
     }
-  }
+  }   
 }
