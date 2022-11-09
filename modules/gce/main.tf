6ffe7815487a14dad62e8c6b14649a6af8177c90
brnpsantos1993 <brnpsantos1993@gmail.com>
2022-11-08 23:17:11 -0300

    some changes

--- modules/gce/main.tf
+++ modules/gce/main.tf
@@ -1,51 +1,54 @@
 provider "google" {
-  project             = var.project
-  region              = var.region
-  alias               = "admin-network"
+  project = var.project
+  region  = var.region
+  alias   = "admin-network"
 }
 data "google_compute_network" "vpc_shared" {
-  name                    = var.vpc_network
-  provider                = google.admin-network
-  project                 = var.project
+  name     = var.vpc_network
+  provider = google.admin-network
+  project  = var.project
 }
 
 data "google_compute_subnetwork" "vpc_subnetwork" {
-    project           = var.project
-    name              = var.vpc_subnetwork
-    region            = var.region
+  project = var.project
+  name    = var.vpc_subnetwork
+  region  = var.region
 }
 
 resource "google_compute_disk" "default" {
-  count = var.secondary_disk == true ? 1 : 0
+  count   = var.secondary_disk == true ? 1 : 0
   project = var.project
-  name = "compute-disk"
-  zone = var.zone
+  name    = "compute-disk"
+  zone    = var.zone
 }
 
 resource "google_compute_instance" "default" {
-  project             = var.project
-  name                = var.compute_name
-  machine_type        = var.machine_type
-  zone                = var.zone
-    boot_disk {
-      initialize_params {
-          image       = var.image
-          size        = var.disk_size
-          type        = var.disk_type
-      }
+  project      = var.project
+  name         = var.compute_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = var.image
+      size  = var.disk_size
+      type  = var.disk_type
     }
+  }
   network_interface {
-    network           = "${data.google_compute_network.vpc_shared.self_link}"
-    subnetwork        = "${data.google_compute_subnetwork.vpc_subnetwork.self_link}"
-    }
+    network    = data.google_compute_network.vpc_shared.self_link
+    subnetwork = data.google_compute_subnetwork.vpc_subnetwork.self_link
+  }
   attached_disk {
-        source = google_compute_disk.default[0].id
-        }
+    source = google_compute_disk.default[0].id
+  }
+  depends_on = [
+    google_compute_disk.default
+  ]
 }
 # resource "google_compute_attached_disk" "default" {
 #   disk     = google_compute_disk.default.id
 #   instance = google_compute_instance.default.id
-   
+
 #   depends_on = [
 #     google_compute_disk.default
 #   ]
--- modules/gce/variables.tf
+++ modules/gce/variables.tf
@@ -1,50 +1,50 @@
 variable "project" {
-    description = "Projeto do GCP"
-    type = string
+  description = "Projeto do GCP"
+  type        = string
 }
 variable "region" {
-    description = "Região do recurso"
-    type = string
+  description = "Região do recurso"
+  type        = string
 }
 variable "vpc_network" {
-    description = "VPC Network"
-    type = string
+  description = "VPC Network"
+  type        = string
 }
 variable "vpc_subnetwork" {
-    description = "VPC Subnetwork"
-    type = string
+  description = "VPC Subnetwork"
+  type        = string
 }
 variable "compute_name" {
-    description = "Nome do Google Compute Engine"
-    type = string
+  description = "Nome do Google Compute Engine"
+  type        = string
 }
 variable "machine_type" {
-    description = "Tipo da máquina do Google Compute Engine"
-    type = string
+  description = "Tipo da máquina do Google Compute Engine"
+  type        = string
 }
 variable "zone" {
-    description = "Zona onde a VM será alocada"
-    type = string
+  description = "Zona onde a VM será alocada"
+  type        = string
 }
 variable "disk_type" {
-    description = "Tipo de disco utilizado no boot"
-    type = string
+  description = "Tipo de disco utilizado no boot"
+  type        = string
 }
 variable "disk_size" {
-    description = "Capacidade do disco"
-    type = number
+  description = "Capacidade do disco"
+  type        = number
 }
 variable "image" {
-    description = "Imagem do sistema Operacional"
-    type = string
+  description = "Imagem do sistema Operacional"
+  type        = string
 }
 
 variable "external_ip" {
-    description = "External IP"
-    type = bool
+  description = "External IP"
+  type        = bool
 }
 
 variable "secondary_disk" {
-    description = ""
-    type = string
+  description = ""
+  type        = string
 }
